8f376faccfb57c50cd621909e5b1dfccdf59e479
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-05-22 10:33:58 +0300

    exercises 1.8 and 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,24 +6,29 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Average = ({good,neutral,bad,total}) => {
+const Average = ({good,neutral,bad,total}) => <div> average {(good*1+neutral*0+bad*(-1))/total}</div>
+  
+
+
+const Positive = (props) => <div>positive {(props.positive/props.total)*100} %</div>
+  
+
+
+const Statistics = ({good,neutral,bad,total}) =>{
   if (total===0) {
     return(
-      <div>average 0</div>
+      <div> No feedback given</div>
     )
   }
   return(
-    <div> average {(good*1+neutral*0+bad*(-1))/total}</div>
-  )
-}
-const Positive = (props) => {
-  if (props.total===0) {
-    return(
-      <div>positive 0</div>
-    )
-  }
-  return(
-    <div>positive {(props.positive/props.total)*100} %</div>
+    <div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {total}</div>
+      <Average good={good} neutral={neutral} bad={bad} total={total} />
+      <Positive positive={good} total={total} />
+    </div>
   )
 }
 
@@ -61,12 +66,7 @@ const App = () => {
       <Button handleClick={handleNeutral} text='neutral'/>
       <Button handleClick={handleBad} text='bad'/>
       <Header text={lowerHeaderText} />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <Average good={good} neutral={neutral} bad={bad} total={total} />
-      <Positive positive={good} total={total} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
     </div>
   )
 
