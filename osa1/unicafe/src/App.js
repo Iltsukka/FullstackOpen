029b2d49c400c4204fb4a2de0be0962c744c5497
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-05-22 10:18:55 +0300

    exercise 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,11 +6,24 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistic = (props)=> {
+const Average = ({good,neutral,bad,total}) => {
+  if (total===0) {
+    return(
+      <div>average 0</div>
+    )
+  }
   return(
-    <div>
-      {props.text} {props.value}
-    </div>
+    <div> average {(good*1+neutral*0+bad*(-1))/total}</div>
+  )
+}
+const Positive = (props) => {
+  if (props.total===0) {
+    return(
+      <div>positive 0</div>
+    )
+  }
+  return(
+    <div>positive {(props.positive/props.total)*100} %</div>
   )
 }
 
@@ -19,28 +32,44 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handleGood = () => setGood(good+1)
-  const handleNeutral = () => setNeutral(neutral+1)
-  const handleBad = () => setBad(bad+1)
+  const [total, setTotal] = useState(0)
+  
+  const handleGood = () => {
+    setGood(good+1)
+    setTotal(total +1)
+    
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setTotal(total+1)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+    setTotal(total+1)
+  }
   const upperHeaderText = 'give feedback'
   const lowerHeaderText = 'statistics'
-  const goodText = 'good'
-  const neutralText = 'neutral'
-  const badText = 'bad'
+  
+  
 
 
   return (
     <div>
+      
       <Header text={upperHeaderText}/>
       <Button handleClick={handleGood} text='good'/>
       <Button handleClick={handleNeutral} text='neutral'/>
       <Button handleClick={handleBad} text='bad'/>
       <Header text={lowerHeaderText} />
-      <Statistic text={goodText} value={good} />
-      <Statistic text={neutralText} value={neutral} />
-      <Statistic text={badText} value={bad}/>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {total}</div>
+      <Average good={good} neutral={neutral} bad={bad} total={total} />
+      <Positive positive={good} total={total} />
     </div>
   )
+
 }
 
 export default App
