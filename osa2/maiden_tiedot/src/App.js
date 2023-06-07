937bc9142a34e6c3c48c37bffe13b9a084f8cc96
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-06-07 12:35:21 +0300

    exercise 2.18

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -7,6 +7,43 @@ const Display = ({country}) => {
   )
 }
 
+const Map = ({list, search}) => {
+  const length = list.length
+  if (length > 10 && search) {
+    return(
+    <div>Too many matches, specify another filter</div>
+    )    
+  }
+
+  if (length === 1) {
+    const oneCountry = list[0]
+    return(
+    <div>
+      <h1>{oneCountry.name.common}</h1>
+      <div>capital {oneCountry.capital}</div>
+      <div>area {oneCountry.area}</div>
+      <h4>languages</h4>
+      <ul>
+        {Object.keys(oneCountry.languages).map((keyName, i) => 
+        <li key={i}>
+          <span>{oneCountry.languages[keyName]}</span>
+        </li>
+        )}
+      </ul>
+      <img style={{border: '1px solid'}} src={list[0].flags.png} height={150} alt='Flag'/>
+    </div> 
+    )
+  }
+    
+  
+  return(
+    <>
+    {list.map(country => 
+      <Display key={country.name.common} country={country}/>)}
+    </>
+    )
+}
+
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
@@ -23,16 +60,15 @@ const App = () => {
       })
   }, [])
 
-  const countriesToMap = () => {
-
-  }
+  const countriesToMap = search
+    ? countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
+    : countries
+  
 
   return (
     <div>
       find countries <input value={search} onChange={handleChange}/>
-      {countries.map(country=> 
-        <Display key={country.name.common} country={country} />
-      )}
+      <Map list={countriesToMap} search={search}/>
     </div>
   )
 }
