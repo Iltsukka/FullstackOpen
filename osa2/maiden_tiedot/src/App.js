57d40c10e009dd1fd9b41ef5e4a7c8f670563ccf
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-06-08 20:16:02 +0300

    exercise 2.19

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,13 +1,40 @@
 import { useState, useEffect } from "react";
 import countryService from './components/countries'
 
-const Display = ({country}) => {
+const Display = ({country, length,action}) => {
+
+  if (length <= 10) {
+    return(
+      <div>{country.name.common} <button onClick={()=>action(country)}>show</button></div>
+    )
+  }
   return(
     <div>{country.name.common}</div>
   )
 }
 
-const Map = ({list, search}) => {
+const CountryInfo = ({country}) => {
+  if (!country) return
+  return(
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h4>languages</h4>
+      <ul>
+        {Object.keys(country.languages).map((keyName, i) => 
+        <li key={i}>
+          <span>{country.languages[keyName]}</span>
+        </li>
+        )}
+      </ul>
+      <img style={{border: '1px solid'}} src={country.flags.png} height={150} alt='Flag'/>
+    </div> 
+    )
+  
+}
+
+const Map = ({list, search, action}) => {
   const length = list.length
   if (length > 10 && search) {
     return(
@@ -39,7 +66,7 @@ const Map = ({list, search}) => {
   return(
     <>
     {list.map(country => 
-      <Display key={country.name.common} country={country}/>)}
+      <Display key={country.name.common} country={country} length={length} action={action}/>)}
     </>
     )
 }
@@ -47,9 +74,13 @@ const Map = ({list, search}) => {
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
+  const [countryToShow, setCountryToShow] = useState(null)
 
   const handleChange = (event) => {
     setSearch(event.target.value)
+    if (countryToShow) {
+      setCountryToShow(null)
+    }
   }
 
   useEffect(()=> {
@@ -64,11 +95,15 @@ const App = () => {
     ? countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
     : countries
   
+  const handleButtonAction = (country) => {
+    setCountryToShow(country)
+  } 
 
   return (
     <div>
       find countries <input value={search} onChange={handleChange}/>
-      <Map list={countriesToMap} search={search}/>
+      <Map list={countriesToMap} search={search} action={handleButtonAction}/>
+      <CountryInfo country={countryToShow}  />
     </div>
   )
 }
